9534e8207ead74f9841480a24e2cfe048d1e846e
nazimfeni <mdnazimuddin77@gmail.com>
2024-01-30 22:03:45 +0600

    info update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Simple Laravel 10 User Roles and Permissions - AllPHPTricks.com</title>
+    <title>User Roles and Permissions - Bionictechbd</title>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
@@ -21,7 +21,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    AllPHPTricks.com
+                    Bionictechbd
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -92,16 +92,16 @@
                             </div>
                         @endif
 
-                        <h3 class="text-center mt-3 mb-3">Simple Laravel 10 User Roles and Permissions - <a href="https://www.allphptricks.com/">AllPHPTricks.com</a></h3>
+                        <h3 class="text-center mt-3 mb-3">User Roles and Permissions - <a href="#">Bionictechbd</a></h3>
                         @yield('content')
                         
                         <div class="row justify-content-center text-center mt-3">
                             <div class="col-md-12">
                                 <p>Back to Tutorial: 
-                                    <a href="https://www.allphptricks.com/simple-laravel-10-user-roles-and-permissions/"><strong>Tutorial Link</strong></a>
+                                    <a href="https://www.bionictechbd.com/"><strong>Tutorial Link</strong></a>
                                 </p>
                                 <p>
-                                    For More Web Development Tutorials Visit: <a href="https://www.allphptricks.com/"><strong>AllPHPTricks.com</strong></a>
+                                    For More Web Development Tutorials Visit: <a href="https://www.bionictechbd.com/"><strong>Bionictechbd</strong></a>
                                 </p>
                             </div>
                         </div>
